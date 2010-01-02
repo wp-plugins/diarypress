434cc0e96d2bd19d4c24d94a6e7aa8894938e35e
tomhowson <tomhowson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-02 23:15:46 +0000

    git-svn-id: https://plugins.svn.wordpress.org/diarypress/trunk@189105 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- diarypress.php
+++ diarypress.php
@@ -1,29 +1,19 @@
 <?php
 /*
-Plugin Name: Diarypress
+Plugin Name: DiaryPress
 Plugin URI: http://www.howson.me.uk/diarypress
-Description: Forces a user to login before viewing a blog. Also allows for a custom logo to be used. The custom logo css part of the plugin is based on the work by binary moon. The idea of the plugin is for people who wish to run their wordpress blog as a diary. The plugin also disables wordpress rss feeds.
-Version: 1.1
+Description: DiaryPress is a plugin designed to allow your blog to operate like a diary. It will disable RSS feeds to keep your blog private and ask you to login in order to access the content. This is handy even if you run your diary on a local web server such as WAMP as it protects your blog against nosey family and friends.
+Version: 2.0
 Author: Tom Howson
 Author URI: http://www.howson.me.uk
 */
 ?>
-
-<?php
-//for the custom login script
-function custom_login() { 
-echo '<link rel="stylesheet" type="text/css" href="' . get_bloginfo('wpurl') . '/wp-content/plugins/diaypress/login.css" />'; 
-}
-add_action('login_head', 'custom_login');
-?>
-
-
 <?php
 	add_action( 'template_redirect', 'force_login' );
 	
 	function force_login()
 	{
-		$redirect_to = $_SERVER['REQUEST_URI']; // Change this line to change to where logging in redirects the user, i.e. '/', 'diary/wp-admin', etc.
+		$redirect_to = $_SERVER['REQUEST_URI']; 
 	
 		if ( ! is_user_logged_in() )
 		{
@@ -42,26 +32,19 @@ add_action('login_head', 'custom_login');
 			else
 			{
 			
-// start of login page
-
-//include get_bloginfo('wpurl') . '/wp-content/plugins/diaypress/splash.php';
-	include "splash.php";	  	
+// die and show error message
+	wp_die( __('<h4><strong>Sorry....</strong></h4> </br><p>You must log in to view this blog. If you want to <a href="'. get_bloginfo('url') .'/wp-admin">Click here</a></p>') );
 
 
 
-
-
-
-				die();
-
-			} // else
+				} // else
 
 	  	} // if
 
 	} // force_login
 	
 	?>
-	<?php
+<?php
 // turn the feeds off for our diary
 function norss() {
 	wp_die( __('<strong>Error:</strong> No RSS Feed Available, Please visit our <a href="'. get_bloginfo('url') .'">homepage</a>.') );
@@ -72,5 +55,4 @@ add_action('do_feed_rdf', 'norss', 1);
 add_action('do_feed_rss', 'norss', 1);
 add_action('do_feed_rss2', 'norss', 1);
 add_action('do_feed_atom', 'norss', 1);
-
 ?>
